ca368d8fb2cb33060d8865b360306dea2bffd515
siers22 <siers6292@gmail.com>
2025-06-13 11:23:49 +0500

    Clients main window added

---
+++ windows/Clients/MainClientsWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace rentPrac1.windows.Clients
+{
+    /// <summary>
+    /// Логика взаимодействия для MainClientsWindow.xaml
+    /// </summary>
+    public partial class MainClientsWindow : Window
+    {
+        public MainClientsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
